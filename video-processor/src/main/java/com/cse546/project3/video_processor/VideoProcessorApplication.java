0a863cabe555555debe25a2476fc54aff2f04d72
mohan <mohankrishnanie@gmail.com>
2024-10-28 14:11:23 -0700

    dummy commit

--- video-processor/src/main/java/com/cse546/project3/video_processor/VideoProcessorApplication.java
+++ video-processor/src/main/java/com/cse546/project3/video_processor/VideoProcessorApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VideoProcessorApplication {
 
+	// Inside the main method of the spring boot application.
 	public static void main(String[] args) {
 		SpringApplication.run(VideoProcessorApplication.class, args);
 	}
