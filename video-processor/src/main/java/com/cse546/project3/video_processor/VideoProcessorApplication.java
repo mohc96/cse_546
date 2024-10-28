11227f7febda08be6800bc8d21e92e8116baaed7
mohan <mohankrishnanie@gmail.com>
2024-10-28 13:59:43 -0700

    first commit

---
+++ video-processor/src/main/java/com/cse546/project3/video_processor/VideoProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.cse546.project3.video_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoProcessorApplication.class, args);
+	}
+
+}
---
+++ video-processor/src/test/java/com/cse546/project3/video_processor/VideoProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cse546.project3.video_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
